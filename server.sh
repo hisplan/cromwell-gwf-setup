3bccd675f7c87c97cf867bd7bce641c9928641a8
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-07-16 15:43:57 -0400

    Add support for different AWS regions, add -i, -k, -r, remove hardcoded

--- server.sh
+++ server.sh
@@ -1,69 +1,85 @@
 #!/bin/bash
 
-# update according to your environment setup
-instance_id="i-0ca914341b4913d6c"
-keypair="~/dpeerlab-chunj.pem"
-
 usage()
 {
 cat << EOF
 USAGE: `basename $0` [options]
     -u  bring up the server instance
     -d  bring down the server instance
+    -i  Cromwell server instance ID
+    -k  path to your EC2 keypair for SSH login
+    -r  AWS region
 EOF
 }
 
 server_up()
 {
     aws ec2 start-instances \
-        --instance-ids ${instance_id}
+        --instance-ids ${instance_id} \
+        --region ${region}
 
     aws ec2 wait instance-status-ok \
-        --instance-ids ${instance_id}
+        --instance-ids ${instance_id} \
+        --region ${region}
 
     # get public DNS name
-    addr=`aws ec2 describe-instances --instance-ids ${instance_id} --query "Reservations[0].Instances[0].{Instance:PublicDnsName}" --output text`
+    addr=`aws ec2 describe-instances --instance-ids ${instance_id} --region ${region} --query "Reservations[0].Instances[0].{Instance:PublicDnsName}" --output text`
 
     echo "Cromwell server address: ${addr}"
 
     ssh -i ${keypair} ec2-user@${addr} ./startup.sh
 
     # send ready message to Slack
-    python3 notify_slack.py \
-        --hook-key="${slack_hook_key}" \
-        --message="Cromwell/Job Manager is available at http://${addr}:4200"
-
+    if [ "$slack_hook_key" != "null" ]
+    then
+        python3 notify_slack.py \
+            --hook-key="${slack_hook_key}" \
+            --message="Cromwell/Job Manager is available at http://${addr}:4200"
+    fi
     echo "Cromwell/Job Manager is available at http://${addr}:4200"
 }
 
 server_down()
 {
     aws ec2 stop-instances \
-        --instance-ids ${instance_id}
+        --instance-ids ${instance_id} \
+        --region ${region}
 
     aws ec2 wait instance-stopped \
-        --instance-ids ${instance_id}
+        --instance-ids ${instance_id} \
+        --region ${region}
 
     # send ready message to Slack
-    python3 notify_slack.py \
-        --hook-key="${slack_hook_key}" \
-        --message="Cromwell/Job Manager is stopped."
-
+    if [ "$slack_hook_key" != "null" ]
+    then
+        python3 notify_slack.py \
+            --hook-key="${slack_hook_key}" \
+            --message="Cromwell/Job Manager is stopped."
+    fi
     echo "Cromwell/Job Manager is stopped."
 }
 
-while getopts "udh" OPTION
+while getopts "udi:k:r:h" OPTION
 do
     case $OPTION in
         u) action="up" ;;
         d) action="down" ;;
+        i) instance_id=$OPTARG ;;
+        k) keypair=$OPTARG ;;
+        r) region=$OPTARG ;;
         h) usage; exit 1 ;;
         *) usage; exit 1 ;;
     esac
 done
 
+if [ -z "$instance_id" ] || [ -z "$keypair" ] || [ -z "$region" ]
+then
+    usage
+    exit 1
+fi
+
 # get slack hook key
-slack_hook_key=`aws ssm get-parameters --name="scata-slack-noti-dev" --query="Parameters[0].Value" | sed 's/"//g'`
+slack_hook_key=`aws ssm get-parameters --name="scata-slack-noti-dev" --query="Parameters[0].Value" --region ${region} | sed 's/"//g'`
 
 if [ "$action" == 'up' ]
 then
